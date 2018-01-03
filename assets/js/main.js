5a87b065c722a857cbd83a7808b861e941e2cdce
Yormi Altamiranda <yormian@gmail.com>
2018-01-03 13:49:47 -0400

    actulaizando los archivos

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     $(window).scroll(function() {
         if ($("#menu").offset().top > 56) {
             $("#menu").addClass("bg-navbar");
-            document.getElementById('logo').src="img/logo-dark.svg";
+            document.getElementById('logo').src="img/logo-2.png";
         } else {
             $("#menu").removeClass("bg-navbar");
-            document.getElementById('logo').src="img/logo-light.svg";
+            document.getElementById('logo').src="img/logo.png";
         }
     });
 	$('.carousel').carousel({interval:5000})
